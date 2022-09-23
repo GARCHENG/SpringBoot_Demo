26d08b163445a6054a6bf1ad1a85c54d881eadb2
GARCHENG <631450632@qq.com>
2022-09-23 19:45:43 +0800

    Initial commit

---
+++ mavenDemo(web项目)/src/main/java/com/gec/App.java
@@ -0,0 +1,13 @@
+package com.gec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class,args);
+    }
+}
